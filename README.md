b6e8c67f03fbc35961fd212e78ad45cc03e83ae0
Camaren Dayton <cydayton@Camarens-MacBook-Pro-155.local>
2019-04-25 10:14:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Starry Sky
+------
+
+### Conceptual Description
+Stars and constellations are constant and trustworthy regardless from where a person is located throughout the world.  In my life, practicing art has been with me since childhood, and I anticipate continuing to involve art in my life in my future career and hobbies as well.  I thought using the night sky would be a fitting theme to present my final project this semester.  When I was considering ideas for my final project, I wanted to create something that brought together several different lessons from throughout the semester.  I was very intrigued by the idea of incorporating human interaction into my piece.  My project involves a face tracking library that takes in the positions of different points on a recognized face.  It then projects these points as twinkling stars into the sky, creating a vision of a face constellation in the sky.  I find a sense of peace in the simplicity and calm motion represented, similar to the feeling of relaxing and star gazing.
+
+
+### Interactive Description
+My piece is very user-friendly.  Ideally, it would be set up on a very large screen to make the viewer feel enveloped into the night sky.  There must be a camera that can focus on the viewer's face and pick up details.  Users can wiggle their faces and move around in front of the camera, and the locations of the stars will be reflected on the screen.  My piece should make any viewer feel relaxed which is accomplished through the smooth flow of the stars and the resemblance to a beautiful, clear night sky.
+
+### Drawing
+![Sketch](assets/sketch.JPG?raw=true "Sketch")
+
+
+### Technical Details
+This project mainly utilizes p5.js to create the canvas and drawings.  I chose to use this library because I wanted to be able to incorporate a elements from different units throughout the semester.  I used scenemanager.js to switch from the title page to project canvas.  The Scene Manager library allows for multiple different pages within one code file.  Within each subgroup in the code, there are separate setup() and draw() functions for each slide.  The moving stars are created with a "Star" class.  I used clmtrackr.js to track the positions of points on the viewer's face.  I then indexed the positions and turned them into twinkling stars.  My project is hosted through GitHub Pages.
+
+[Here's a link to the piece](https://cydayton.github.io/finalproject/)
---
+++ sketch.js
@@ -0,0 +1,257 @@
+var ctracker;
+var slider;
+var cfont;
+var ifont;
+statstars = [];
+var statstar;
+
+
+var mgr;
+
+function setup()
+{
+    createCanvas(windowWidth, windowHeight);
+
+    mgr = new SceneManager();
+
+    // Preload scenes. Preloading is normally optional
+    // ... but needed if showNextScene() is used.
+    mgr.addScene(Intro);
+    mgr.addScene(Planetarium);
+
+    mgr.showNextScene();
+
+    cfont = loadFont('assets/weather.otf');
+    ifont = loadFont('assets/courier.ttf');
+}
+
+function draw()
+{
+    mgr.draw();
+}
+
+function mousePressed()
+{
+    mgr.handleEvent("mousePressed");
+}
+
+// Intro scene constructor function
+function Intro(){
+  class StatStar {
+    constructor() {
+      this.x = random(-width, width);
+        this.y = random(-height, height);
+        this.z = random(width);      
+        this.pz = this.z;
+    }
+    update() {
+      this.z = this.z - speed;
+      if (this.z < 1) {
+        this.z = width/2;
+        this.x = random(-width, width);
+        this.y = random(-height, height);
+        this.pz = this.z;
+      }
+    }
+  
+    show() {
+      fill(255);
+      noStroke();
+      
+      var sx = map(this.x/this.z, 0, 1, 0, width);
+      var sy = map(this.y/this.z, 0, 1, 0, height);
+      var r = map(this.z, 0, width, 12, 0);
+      image(star1, sx, sy, r, r);    
+      
+      var px = map(this.x/this.pz, 0, 1, 0, width);
+      var py = map(this.y/this.pz, 0, 1, 0, height);
+      this.pz = this.z;
+      
+      stroke(255);
+      line(px, py, sx, sy);
+    }}
+
+    this.setup = function() {
+      var cnv = createCanvas(windowWidth, windowHeight);
+
+      star1 = loadImage('assets/star1.png')
+
+      statstar = new StatStar();
+      // Create an array of 1600 star objects
+      for (var i = 0; i < 1600; i++) {
+          statstars[i] = new StatStar();
+      }
+    }
+
+    this.draw = function() {
+      background(0);
+      fill(255);
+      textAlign(CENTER);
+      textSize(72);
+      textFont(cfont);
+      text("Starry Sky", width/2, -100+height/2);
+
+      textSize(24);
+      textFont(ifont);
+      text("To venture to the planetarium, click anywhere.", width/2, height/2);
+      text("Once inside, move the mouse to watch the stars fly.", width/2, 50+height/2);
+      text("Hover over the constellations to learn their names.", width/2, 100+height/2);
+      text("Drag the slider to change the size of your own personal constellation.", width/2, 150+height/2);
+
+      textSize(14);
+      text("A project by Camaren Dayton", width/2, 250+height/2);
+
+      speed = map(mouseX, 0, width, 2, 10);
+      translate(width/2, height/2);
+      
+      for (var i = 0; i < statstars.length; i++) {
+        statstars[i].update();
+          statstars[i].show();
+    }
+
+    this.mousePressed = function() {
+        // switch the scene
+        this.sceneManager.showScene(Planetarium);
+    }
+}
+}
+
+// Main scene constructor function
+function Planetarium(){
+  var stars = [];
+  var poslocs = [];
+  var speed;
+  class Star {
+      constructor() {
+        this.x = random(-width, width);
+          this.y = random(-height, height);
+          this.z = random(width);      
+          this.pz = this.z;
+      }
+      update() {
+        this.z = this.z - speed;
+        if (this.z < 1) {
+          this.z = width/2;
+          this.x = random(-width, width);
+          this.y = random(-height, height);
+          this.pz = this.z;
+        }
+      }
+    
+      show() {
+        fill(255);
+        noStroke();
+        
+        var sx = map(this.x/this.z, 0, 1, 0, width);
+        var sy = map(this.y/this.z, 0, 1, 0, height);
+        var r = map(this.z, 0, width, 12, 0);
+        image(star1, sx, sy, r, r);    
+        
+        var px = map(this.x/this.pz, 0, 1, 0, width);
+        var py = map(this.y/this.pz, 0, 1, 0, height);
+        this.pz = this.z;
+        
+        stroke(255);
+        line(px, py, sx, sy);
+      }
+  }
+
+    this.setup = function() {
+      // setup camera capture
+      var videoInput = createCapture();
+      videoInput.size(windowWidth, windowHeight);
+      videoInput.position(0, 0);
+      
+      // setup canvas
+      var cnv = createCanvas(windowWidth, windowHeight);
+      cnv.position(0, 0);
+
+      // setup tracker
+      ctracker = new clm.tracker();
+      ctracker.init(pModel);
+      ctracker.start(videoInput.elt);
+
+      slider = createSlider(0, 50, 10);
+      slider.position(0, height-20);
+
+      noStroke();
+
+      star = new Star();
+      // Create an array of 1600 star objects
+      for (var i = 0; i < 1600; i++) {
+          stars[i] = new Star();
+      }
+    }
+
+    this.draw = function() {
+      background(0, 15, 36);
+
+      // get array of face marker positions
+      var positions = ctracker.getCurrentPosition();
+      
+      for (var i=0; i<positions.length; i++) {
+        // draw star at each position point
+        var val = slider.value();
+        image(star1, positions[i][0], positions[i][1], val, val);
+      }
+      push();
+      translate(200, 100);
+      // draw constellations
+      stroke(255);
+      strokeWeight(0.5);
+      line(100,100,150,80);//cygnus
+      line(155,78,175,70);
+      line(175,65,165,40);
+      line(180,70,200,70);
+      line(176,75,175,105);
+      if ((mouseX > 300) && (mouseX < 400) && (mouseY > 150) && (mouseY < 205)) {
+        text("Cygnus", 150, 80)
+      } 
+      
+      line(300,300,200,400);//bigdipper
+      line(200,405,340,460);
+      line(340,455,345,310);
+      line(340,305,305,300);
+      line(345,300,380,260);
+      line(385,255,450,230);
+      if ((mouseX > 400) && (mouseX < 650) && (mouseY > 360) && (mouseY < 550)) {
+        text("Big Dipper", 260, 400)
+      } 
+      
+      line(230,200,250,180);//cancer
+      if ((mouseX > 430) && (mouseX < 450) && (mouseY > 280) && (mouseY < 300)) {
+        text("Cancer", 240, 190)
+      } 
+      
+      line(80,350,100,350);//orionsbelt
+      line(105,350,125,350);
+      line(75,355,30,425);
+      line(35,427,155,440);
+      line(155,435,125,355);
+      line(75,345,40,280);
+      line(45,275,155,290);
+      line(160,295,125,345);
+      if ((mouseX > 235) && (mouseX < 355) && (mouseY > 380) && (mouseY < 540)) {
+        text("Orion's Belt", 60, 340)
+      } 
+      
+      line(480,150,440,100);//cepheus
+      line(540,90,500,40);
+      line(485,150,540,95);
+      line(440,95,448,30);
+      line(448,25,495,40);
+      if ((mouseX > 640) && (mouseX < 740) && (mouseY > 125) && (mouseY < 250)) {
+        text("Cepheus", 460, 80)
+      } 
+      pop();
+
+      // move stars on canvas
+      speed = map(mouseX, 0, width, 2, 10);
+      translate(width/2, height/2);
+      
+      for (var i = 0; i < stars.length; i++) {
+        stars[i].update();
+          stars[i].show();
+      }
+    }
+  }
