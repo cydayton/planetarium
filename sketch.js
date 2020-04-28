4f0812df877182c9f5b21aede864cf62b1213685
Camaren Dayton <cydayton@Camarens-MacBook-Pro-155.local>
2020-04-28 12:27:42 -0400

    update

--- sketch.js
+++ sketch.js
@@ -35,6 +35,11 @@ function mousePressed()
     mgr.handleEvent("mousePressed");
 }
 
+function keyPressed()
+{
+    mgr.handleEvent("keyPressed");
+}
+
 // Intro scene constructor function
 function Intro(){
   class StatStar {
@@ -84,6 +89,7 @@ function Intro(){
     }
 
     this.draw = function() {
+      cursor(ARROW);
       background(0);
       fill(255);
       textAlign(CENTER);
@@ -93,10 +99,10 @@ function Intro(){
 
       textSize(24);
       textFont(ifont);
-      text("To venture to the planetarium, click anywhere.", width/2, height/2);
-      text("Once inside, move the mouse to watch the stars fly.", width/2, 50+height/2);
-      text("Hover over the constellations to learn their names.", width/2, 100+height/2);
-      text("Drag the slider to change the size of your own personal constellation.", width/2, 150+height/2);
+      text("Move your cursor to change the speed of the flying stars.", width/2, height/2);
+      text("To venture inside the planetarium, click anywhere in the sky...", width/2, 50+height/2);
+      text("Allow camera access to create your own personal constellation!", width/2, 100+height/2);
+      text("Once inside, hover over the constellations to learn their names.", width/2, 150+height/2);
 
       textSize(14);
       text("A project by Camaren Dayton", width/2, 250+height/2);
@@ -113,6 +119,7 @@ function Intro(){
         // switch the scene
         this.sceneManager.showScene(Planetarium);
     }
+
 }
 }
 
@@ -171,7 +178,7 @@ function Planetarium(){
       ctracker.init(pModel);
       ctracker.start(videoInput.elt);
 
-      slider = createSlider(0, 50, 10);
+      slider = createSlider(1, 50, 10);
       slider.position(0, height-20);
 
       noStroke();
@@ -185,6 +192,25 @@ function Planetarium(){
 
     this.draw = function() {
       background(0, 15, 36);
+      // back arrow
+      push();
+      beginShape();
+      stroke(150, 255, 255);
+      vertex(30, 15);
+      vertex(15, 30);
+      vertex(30, 45);
+      vertex(15, 30);
+      vertex(30, 15);
+      endShape();
+      beginShape();
+      vertex(25, 30);
+      vertex(60, 30);
+      vertex(25, 30);
+      endShape();
+      pop();
+      if((mouseX<120) && (mouseX>15) && (mouseY<45) && (mouseY>15)){
+        cursor(HAND);
+      } else{cursor(ARROW);}
 
       // get array of face marker positions
       var positions = ctracker.getCurrentPosition();
@@ -194,42 +220,47 @@ function Planetarium(){
         var val = slider.value();
         image(star1, positions[i][0], positions[i][1], val, val);
       }
-      push();
-      translate(200, 100);
+
       // draw constellations
       stroke(255);
-      strokeWeight(0.5);
+      strokeWeight(1);
+
+      push();
+      translate(windowWidth/10, windowHeight/8);
       line(100,100,150,80);//cygnus
       line(155,78,175,70);
       line(175,65,165,40);
       line(180,70,200,70);
       line(176,75,175,105);
-      if ((mouseX > 300) && (mouseX < 400) && (mouseY > 150) && (mouseY < 205)) {
+      if ((mouseX > 100 + windowWidth/10) && (mouseX < 420 + windowWidth/10) && (mouseY > 50 + windowHeight/8) && (mouseY < 100 + windowHeight/8)) {
         text("Cygnus", 150, 80)
       } 
+      pop();
+
       push();
-      translate(1200, 300)
-      line(300,300,200,400);//bigdipper
-      line(200,405,340,460);
-      line(340,455,345,310);
-      line(340,305,305,300);
-      line(345,300,380,260);
-      line(385,255,450,230);
-      if ((mouseX > 1600) && (mouseX < 1850) && (mouseY > 660) && (mouseY < 850)) {
-        text("Big Dipper", 260, 400)
+      translate(3*windowWidth/4, 5*windowHeight/8)
+      line(0, 10, 80, 30); //big dipper
+      line(85, 35, 115, 65);
+      line(120, 70, 155, 100);
+      line(160, 105, 290, 150);
+      line(155, 110, 145, 165);
+      line(150, 170, 250, 220);
+      line(255, 220, 295, 155);
+      if ((mouseX > 3*windowWidth/4) && (mouseX < 300 + 3*windowWidth/4) && (mouseY > 5*windowHeight/8) && (mouseY < 200 + 5*windowHeight/8)) {
+        text("Big Dipper", 100, 100)
       } 
       pop();
       
       push();
-      translate(500, 0)
-      line(230,200,250,180);//cancer
-      if ((mouseX > 930) && (mouseX < 950) && (mouseY > 280) && (mouseY < 300)) {
-        text("Cancer", 240, 190)
+      translate(windowWidth/3, windowHeight/3)
+      line(50,0,70,20);//cancer
+      if ((mouseX > 50 + windowWidth/3) && (mouseX < 70 + windowWidth/3) && (mouseY > windowHeight/3) && (mouseY < 20 + windowHeight/3)) {
+        text("Cancer", 60, 10)
       } 
       pop();
       
       push();
-      translate(0, 500);
+      translate(windowWidth/12, windowHeight/5);
       line(80,350,100,350);//orionsbelt
       line(105,350,125,350);
       line(75,355,30,425);
@@ -238,19 +269,19 @@ function Planetarium(){
       line(75,345,40,280);
       line(45,275,155,290);
       line(160,295,125,345);
-      if ((mouseX > 235) && (mouseX < 355) && (mouseY > 880) && (mouseY < 940)) {
+      if ((mouseX > 40 + windowWidth/12) && (mouseX < 150 + windowWidth/12) && (mouseY > 280 + windowHeight/5) && (mouseY < 440 + windowHeight/5)) {
         text("Orion's Belt", 60, 340)
       } 
       pop();
       
       push();
-      translate(800, 100);
+      translate(windowWidth/3, 80);
       line(480,150,440,100);//cepheus
       line(540,90,500,40);
       line(485,150,540,95);
       line(440,95,448,30);
       line(448,25,495,40);
-      if ((mouseX > 1440) && (mouseX < 1540) && (mouseY > 225) && (mouseY < 350)) {
+      if ((mouseX > 440 + windowWidth/3) && (mouseX < 540 + windowWidth/3) && (mouseY > 30 + 80) && (mouseY < 150 + 80)) {
         text("Cepheus", 460, 80)
       } 
       pop();
@@ -263,5 +294,20 @@ function Planetarium(){
         stars[i].update();
           stars[i].show();
       }
+
+      this.keyPressed = function() {
+        // switch the scene
+        if (keyCode === LEFT_ARROW) {
+          this.sceneManager.showScene(Intro);
+        }
+      }
+      this.mousePressed = function() {
+        if((mouseX<120) && (mouseX>15) && (mouseY<45) && (mouseY>15)){
+        // switch the scene
+        this.sceneManager.showScene(Intro);
+      }
+    }
     }
   }
+
+
